fa557439f78e335ebe6b5cf92f1b93812f4ea52f
cyberpete2244 <shqfaya@gmail.com>
2023-01-22 01:38:14 +0100

    wadsfa

---
+++ ebAlert/core/configs.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+
+class Configs:
+    LOGGING = os.environ.get("LOGGING") or logging.ERROR
+    BOTTOKEN = os.environ.get("BOTTOKEN") or "BOTTOKEN"
+    LOCATION_FILTER = ""  # example: "distance1,zip11,zip12,...zip1N-dist2,zip21,...,zip2N[...]" or ""
+    CHAT_ID = os.environ.get("CHAT_ID") or "CHAT_ID"  # ID for test bot
+    FILE_LOCATION = os.path.join(os.path.expanduser("~"), "ebayklein.db")
+    SOURCE_INDICATOR = ""  # OPTIONAL: first characters of telegram message
+
+
+configs = Configs()
